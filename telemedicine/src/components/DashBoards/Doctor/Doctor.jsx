98f552479ad7ea0b74ba5f773ab13b7818b7112d
unknown <12162150@cqumail.com>
2024-05-09 19:45:21 +0300

    created doctor dashboard

--- telemedicine/src/components/DashBoards/Doctor/Doctor.jsx
+++ telemedicine/src/components/DashBoards/Doctor/Doctor.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React from 'react';
+import './Doctor.css';
+import { Link } from 'react-router-dom';
+
 
 const Doctor = () => {
   return (
-    <div>Doctor Dash</div>
+    <div className='ddbody'>
+      <h1 className='ddtitle'>Doctor Dashboard</h1>
+
+      <div className="dddosage">
+          <div className="content">
+            <Link to="/patient-history" className='ddcomplink'><u> Meet </u> </Link><br /> <br />
+          </div>
+
+        </div>
+        <br />
+<div></div>
+      <div className="ddcomponents">
+      <div className="ddmedicalhistory">
+          <div className="ddcontent">
+            <Link to="/patient-history" className='ddcomplink'><u> Dosage & Prescription </u> </Link><br /> <br />
+            <p>
+              This Page will allow the patient supply the symptoms and get preliminary analysis.
+            </p>
+
+
+          </div>
+
+        </div>
+        <div className="ddmedicalhistory">
+          <div className="ddcontent">
+            <Link to="/patient-history" className='ddcomplink'><u> Medical History </u> </Link><br /> <br />
+            <p>
+              This Page will list all the Medical History of diffferent Patients.
+            </p>
+
+
+          </div>
+
+        </div>
+        <div className="ddmedicalhistory">
+          <div className="ddcontent">
+            <Link to="/book-appointment" className='ddcomplink'><u> Appointments </u> </Link><br /><br />
+            <p>
+              This Page will list all the information about Appointments.
+            </p>
+
+
+          </div>
+
+        </div>
+        
+        <div className="ddmedicalhistory">
+          <div className="ddcontent">
+            <Link to="/symptoms-checker" className='ddcomplink'><u> Ratings & Reviews </u> </Link><br /> <br />
+            <p>
+              This Page will list all the feedbacks left by patients after service delivery.
+            </p>
+
+
+          </div>
+
+        </div>
+      </div>
+   
+
+    </div>
   )
 }
 
